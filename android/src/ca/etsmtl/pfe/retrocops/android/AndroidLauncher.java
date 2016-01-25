e4190af624502f2a7734fdbcb6a1b3b8c030b02b
SGV <b0ul@users.noreply.github.com>
2016-01-25 15:58:19 -0500

    initial commit

---
+++ android/src/ca/etsmtl/pfe/retrocops/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package ca.etsmtl.pfe.retrocops.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ca.etsmtl.pfe.retrocops.RetroCops;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new RetroCops(), config);
+	}
+}
